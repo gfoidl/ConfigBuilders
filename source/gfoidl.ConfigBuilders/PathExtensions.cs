bad0abc24f560366ef54bf05aadd5ee8dd5bb71d
Günther Foidl <gue@korporal.at>
2018-03-16 14:49:06 +0100

    PathExtensions added

---
+++ source/gfoidl.ConfigBuilders/PathExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace gfoidl.ConfigBuilders
+{
+    internal static class PathExtensions
+    {
+        public static string InsertMachineName(this string path)
+        {
+            var pathSegments      = new PathSegments(path);
+            pathSegments.FileName = $"{pathSegments.FileName}_{Environment.MachineName}";
+
+            return pathSegments.GetFullPath();
+        }
+        //---------------------------------------------------------------------
+        internal struct PathSegments
+        {
+            public string Folders   { get; set; }
+            public string FileName  { get; set; }
+            public string Extension { get; set; }
+            //-----------------------------------------------------------------
+            public PathSegments(string path)
+            {
+                this.Folders   = Path.GetDirectoryName(path);
+                this.FileName  = Path.GetFileNameWithoutExtension(path);
+                this.Extension = Path.GetExtension(path);
+            }
+            //-----------------------------------------------------------------
+            public string GetFullPath() => Path.Combine(this.Folders, Path.ChangeExtension(this.FileName, this.Extension));
+        }
+    }
+}
---
+++ tests/gfoidl.ConfigBuilders.Tests/PathExtensionsTests/InsertMachineName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace gfoidl.ConfigBuilders.Tests.PathExtensionsTests
+{
+    [TestFixture]
+    public class InsertMachineName
+    {
+        [Test]
+        public void FileName_given([Values("svclog", "log", "txt", "bar")]string extension)
+        {
+            string path     = Path.ChangeExtension("mylog", extension);
+            string expected = Path.ChangeExtension($"mylog_{Environment.MachineName}", extension);
+
+            string actual = path.InsertMachineName();
+
+            Assert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void FileName_with_absolute_path_given([Values("svclog", "log", "txt", "bar")]string extension)
+        {
+            string path     = Path.ChangeExtension(Path.Combine(@"k:\", "logs", "mylog"), extension);
+            string expected = Path.ChangeExtension(Path.Combine(@"k:\", "logs", $"mylog_{Environment.MachineName}"), extension);
+
+            string actual = path.InsertMachineName();
+
+            Assert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void FileName_with_relative_path_given([Values("svclog", "log", "txt", "bar")]string extension)
+        {
+            string path     = Path.ChangeExtension(Path.Combine(@"..\", "logs", "mylog"), extension);
+            string expected = Path.ChangeExtension(Path.Combine(@"..\", "logs", $"mylog_{Environment.MachineName}"), extension);
+
+            string actual = path.InsertMachineName();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
